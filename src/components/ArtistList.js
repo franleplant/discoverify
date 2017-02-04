1fefc3be719f29257d5dbacd69179253b5223770
Fran Guijarro <franleplant@gmail.com>
2017-02-03 23:35:55 -0300

    Use ListView in ArtistList

--- src/components/ArtistList.js
+++ src/components/ArtistList.js
@@ -7,35 +7,71 @@ import {
   View,
   TouchableHighlight,
   Image,
+  ListView,
 } from 'react-native';
 import {Actions as RouterActions} from 'react-native-router-flux';
-import type { Artist, Track } from '../types'
+import type { Artist, Track, ArtistTrackMap } from '../types'
 import ArtistListItem from './ArtistListItem'
 
 type Props = {
   artists: Array<Artist>,
-  topTracks?: {
-    [artistId: string]: Array<Track>,
-  },
+  topTracks?: ArtistTrackMap,
   limit?: number,
   title?: string,
 }
 
-export default function ArtistList(props: Props) {
-  const {limit = 5, title = 'Artists', topTracks = {}} = props;
+type State = {
+  dataSource: any;
+}
 
-  if (props.artists.length === 0) {
-    return null;
+export default class ArtistList extends React.Component {
+  props: Props;
+  state: State;
+
+  static defaultProps = {
+    limit: 5,
+    title: 'Artists',
+    topTracks: {},
+  };
+
+  constructor(...args: Array<any>) {
+    super(...args);
+    this.initDataSource(this.props);
   }
 
-  return (
-    <View style={{}}>
-      <Text style={styles.title}>{title}</Text>
-      {props.artists.slice(0, limit).map((artist, index) => (
-        <ArtistListItem key={index} artist={artist} topTracks={topTracks[artist.id]} />
-      ))}
-    </View>
-  )
+  componentWillReceiveProps(nextProps: Props) {
+    if (nextProps.artists.length !== this.props.artists.length) {
+      this.initDataSource(nextProps);
+    }
+  }
+
+  initDataSource(props: Props) {
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      dataSource: ds.cloneWithRows(props.artists.slice(0, props.limit))
+    };
+  }
+
+  render() {
+    const {title, topTracks, artists} = this.props;
+
+    if (artists.length === 0) {
+      return null;
+    }
+
+    return (
+      <View style={{}}>
+        <Text style={styles.title}>{title}</Text>
+        <ListView
+          enableEmptySections={true}
+          dataSource={this.state.dataSource}
+          renderRow={(artist) => (
+            <ArtistListItem artist={artist} topTracks={topTracks ? topTracks[artist.id] : []} />
+          )}
+        />
+      </View>
+    )
+  }
 }
 
 
